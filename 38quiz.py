106502c053c68236f62bb75324181b18e500a65b
sonnercuen <conner.n.suen321@gmail.com>
2024-02-06 09:31:09 -0800

    final quiz

--- 38quiz.py
+++ 38quiz.py
@@ -1,1 +1,17 @@
-#Conner Suen and Ahbi Sharma
+#Quiz by Conner Suen and Ahbi Sharma
+
+denom = 1
+pi = 1
+sign = 1
+def estimate(n):
+	for i in range(1,n):
+		denom = i*2 + 1
+		x = pi - (1 / denom) 
+		sign = i * -1 
+		x*4
+	return x
+answer = estimate(100)
+print(answer)
+	
+		
+	
