061c38817e199f85b37e8e0cb586941ee2f63bd6
sonnercuen <conner.n.suen321@gmail.com>
2024-02-10 00:21:43 -0800

    unit 4 hw

---
+++ 41zscores.py
@@ -0,0 +1,16 @@
+# 41zscores.py by Conner Suen
+
+import random
+
+z1 = 0
+z2 = 0
+z3 = 0
+limit = 100000
+for i in range(limit):
+	r = random.gauss(0.0, 1.0)
+	if r > 1: z1 += 1
+	if r > 2: z2 += 1
+	if r > 3: z3 += 1
+print(1 - 2 * z1 / limit)
+print(1 - 2 * z2 / limit)
+print(1 - 2 * z3 / limit)
---
+++ 42chicago.py
@@ -0,0 +1,34 @@
+# 42chicago.py by Conner Suen
+
+import random
+
+games = 1000
+'''
+for i in range(games):
+	print(f'game #{i}')
+	for target in range(2, 13):
+		d1 = random.randint(1, 6)
+		d2 = random.randint(1, 6)
+		if d1 + d2 == target:
+			print(f'yay, rolled {d1} and {d2} for {target} points')
+
+for i in range(games):
+	score = 0
+	for target in range(2, 13):
+		if random.randint(1, 6) + random.randint(1, 6) == target:
+			score += target
+		print(score)
+'''	
+log = games // 100
+total = 0
+zeroes = 0
+for i in range(games):
+	if i % log == 0: print(f'{100 * i / games:.0f}')
+	score = 0
+	for target in range(2, 13):
+		if random.randint(1, 6) + random.randint(1, 6) == target:
+			score += target
+	if score == 0: zeroes += 1
+	total += score
+print(total / games)
+print(zeroes / games)
