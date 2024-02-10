37c2654b4bd749f486184db6e1ef8a35e1fac5dc
sonnercuen <conner.n.suen321@gmail.com>
2024-02-10 11:17:46 -0800

    47 update

--- 47deathsaves.py
+++ 47deathsaves.py
@@ -2,34 +2,24 @@
 
 import random
 
-success = 0
-failure = 0
-d = 0
-s = 0
-r = 0
-n = 0
 death = 0
 live = 0
 res = 0
 roll = 1000
 for i in range(roll):
+	success = 0
+	failure = 0
 	while True:
 		d20 = random.randint(1, 20)
-		n += 1
 		if   d20 == 1: 
 			failure += 2
-			d += 2
 		elif d20 == 20: 
-			success = 3
-			r += 1
 			res += 1
 			break
 		elif d20 < 10: 
 			failure += 1
-			d += 1
 		elif d20 >= 10: 
 			success += 1
-			s += 1
 		if success == 3: 
 			live += 1
 			break
